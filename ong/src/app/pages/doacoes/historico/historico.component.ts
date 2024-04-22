0c790f48d6f30ce2fc8467273f1ce754f7c44be5
CaueFer <cauefernandescaetano@gmail.com>
2024-04-21 21:33:24 -0300

    add skeleton to historico

--- ong/src/app/pages/doacoes/historico/historico.component.ts
+++ ong/src/app/pages/doacoes/historico/historico.component.ts
@@ -1,11 +1,42 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { DoacaoModel } from "../gerenciador/doacao.model";
+import { HistoricoModel } from "./historico.model";
+import { setTime } from "ngx-bootstrap/chronos/utils/date-setters";
 
 @Component({
-  selector: 'app-historico',
-  templateUrl: './historico.component.html',
-  styleUrls: ['./historico.component.scss']
+  selector: "app-historico",
+  templateUrl: "./historico.component.html",
+  styleUrls: ["./historico.component.scss"],
 })
 export class HistoricoComponent {
-isActive: any;
+  masterSelected!: boolean;
 
+  historico: HistoricoModel[] = [];
+
+  constructor() {
+    setTimeout(() =>{
+      this.historico = [{
+        id: "02",
+        categoria: "Objeto",
+        itemName: "Armario",
+        data: "20/02/2024",
+        qntd: 12,
+        tipoMov: "entrada",
+        doadorName: "Jailson"
+      }, {
+        id: "02",
+        categoria: "Objeto",
+        itemName: "Armario",
+        data: "20/02/2024",
+        qntd: 12,
+        tipoMov: "entrada",
+        doadorName: "Jailson"
+      }];
+    }, 2000)
+  }
+
+  checkUncheckAll($event: any) {
+    throw new Error("Method not implemented.");
+  }
+  isActive: any;
 }
---
+++ ong/src/app/pages/doacoes/historico/historico.model.ts
@@ -0,0 +1,9 @@
+export interface HistoricoModel {
+    id?: any;
+    categoria?: string;
+    itemName?: string;
+    data?: string;
+    qntd?: number;
+    tipoMov?: any;
+    doadorName?: any;
+}
