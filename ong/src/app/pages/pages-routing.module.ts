c07e34a72bfa107926437726968ae0a3712de160
CaueFer <cauefernandescaetano@gmail.com>
2024-04-29 20:45:18 -0300

    initial dashboard page responsive

--- ong/src/app/pages/pages-routing.module.ts
+++ ong/src/app/pages/pages-routing.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { DefaultComponent } from './dashboards/dashboard/default.component';
 import { GerenciadorComponent } from './doacoes/gerenciador/gerenciador.component';
 import { HistoricoComponent } from './doacoes/historico/historico.component';
 import { SingleHistoricoComponent } from './doacoes/single-historico/single-historico.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
-  { path: 'dashboard', component: DefaultComponent },
+  { path: 'dashboard', component: DashboardComponent },
   { path: 'gerenciador', component: GerenciadorComponent },
   { path: 'historico', component: HistoricoComponent },
   { path: 'historico-single', component: SingleHistoricoComponent },
