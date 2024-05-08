426db0eab98f51d70b2a518d1f02ef98ddc3a6e7
CaueFer <cauefernandescaetano@gmail.com>
2024-05-08 15:55:24 -0300

    MINOR UPDATE

---
+++ ong/src/app/pages/familias/family-dashboard/family-dashboard.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-family-dashboard',
+  standalone: true,
+  imports: [],
+  templateUrl: './family-dashboard.component.html',
+  styleUrl: './family-dashboard.component.scss'
+})
+export class FamilyDashboardComponent {
+
+}
---
+++ ong/src/app/pages/familias/single-family/single-family.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-single-family',
+  standalone: true,
+  imports: [],
+  templateUrl: './single-family.component.html',
+  styleUrl: './single-family.component.scss'
+})
+export class SingleFamilyComponent {
+
+}
